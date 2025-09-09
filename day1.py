b20a4a602a6ff9f81dd1cf4ae31542c585a4b058
Kolbitri <kolbitri2@gmail.com>
2025-09-09 15:58:48 +0200

    day1

---
+++ day1.py
@@ -0,0 +1,17 @@
+# --- BLOK 1: Hello World ---
+print("Hello, World!")
+
+# --- BLOK 2: Variables ---
+name = "Konrad"
+age = 21
+print("Cześć,", name)
+
+# --- BLOK 3: math ops ---
+a = 11
+b = 45
+print("Dodawanie:", a + b)
+
+# --- BLOK 4: Input exaple ---
+imie = input("Jak masz na imię? ")
+wiek = int(input("Ile masz lat? "))
+print("Cześć,", imie)
