a6c6979eea047503606a61fdff087ed2b74dbb0c
Kolbitri <kolbitri2@gmail.com>
2025-09-09 16:28:53 +0200

    Day 1: variables, math, input and age calculator

--- day1.py
+++ day1.py
@@ -4,14 +4,20 @@ print("Hello, World!")
 # --- BLOK 2: Variables ---
 name = "Konrad"
 age = 21
-print("Cześć,", name)
+print("HeyKonra,", name)
 
 # --- BLOK 3: math ops ---
 a = 11
 b = 45
-print("Dodawanie:", a + b)
+print("addition:", a + b)
 
 # --- BLOK 4: Input exaple ---
-imie = input("Jak masz na imię? ")
-wiek = int(input("Ile masz lat? "))
-print("Cześć,", imie)
+name2 = input("Whats your name? ")
+age2 = int(input("How old are you? "))
+print("Cześć,", name2)
+
+# --- BLOK 5: combined ---
+name3 = input("Whats your name? ")
+datebirth = int(input("What year were you born?" ))
+datecuurent = int(input("What is the current year?"))
+print("Hello", name3, ".You were born in", datebirth, "and in this year you are/will be", datecuurent - datebirth,"years old" )
